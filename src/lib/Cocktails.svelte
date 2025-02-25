42fcdb50733484e22d476178dd06b5d2b36f2ecc
Bryan Gordon <bryanbge92@gmail.com>
2025-02-24 19:37:21 -0500

    Create component

---
+++ src/lib/Cocktails.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let cocktailData
+</script>
+
+<article>
+  <div class="card-container">
+
+    <div class="img-container">
+      <img src={cocktailData.strDrinkThumb} alt={cocktailData.strDrink}/>
+    </div>
+
+    <div>
+      <h4>{cocktailData.strDrink}</h4>
+      <p>{cocktailData.strInstructions}</p>
+    </div>
+
+  </div>
+</article>
+
+<style>
+  
+</style>
